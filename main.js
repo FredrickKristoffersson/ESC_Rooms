d5ac959bc16b951e8d386c4ae04e8b80178488e2
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2023-11-11 14:31:20 +0100

    Starting over with menu

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-const menuBg = document.querySelector(".nav__mobile--bg");
-const mobileMenu = document.querySelector(".nav__mobile--menu");
-const hamburgerButton = document.querySelector(".nav__mobile--openMenu");
-const closeMobileMenu = document.querySelector(".nav__mobile--closeMenu");
-const queryHtmlEle = document.querySelector("html");
-const hamburgerMenuLinks = document.querySelectorAll(".hamburgerLink");
+const menuBtn = document.querySelector("#btn");
+const body = document.querySelector("body");
 
-console.log(hamburgerMenuLinks.length)
+menuBtn.addEventListener("click", changeColor);
 
-
-hamburgerButton.addEventListener("click", runOpenMenu);
-closeMobileMenu.addEventListener("click", runCloseMenu);
-hamburgerMenuLinks.forEach(link => {
-  link.addEventListener("click", runCloseMenu);
-});
-
-
-function runOpenMenu() {
-  queryHtmlEle.style.overflow = "hidden"
-  runOpenAndClose("flex");
-
-}
-
-function runCloseMenu() {
-  runOpenAndClose("none");
-  queryHtmlEle.style.removeProperty("overflow");
-}
-
-function runOpenAndClose(property) {
-  menuBg.style.display = property;
-  mobileMenu.style.display = property;
-
+function changeColor(){
+    body.style.backgroundColor = "#000";
 }
