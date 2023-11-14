f8aa209fbb2aa5fe1a46a5d0604701996413f014
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2023-11-14 13:28:54 +0100

    Added function to close mobilemenu icon

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const mobileMenu = document.querySelector(".header_menuBtn");
+const closeMenu = document.querySelector(".closeBtn");
 
 mobileMenu.addEventListener("click", () => {
   console.log("hej det fungerar");
   document.querySelector(".mobileMenu").classList.add("mobileMenu_open");
   document.querySelector(".mobileMenu_ul").classList.add("mobileMenu_ul_open");
 });
+
+closeMenu.addEventListener("click", () => {
+  document.querySelector(".mobileMenu").classList.remove("mobileMenu_open");
+  document.querySelector(".mobileMenu_ul").classList.remove("mobileMenu_ul_open");
+})
+
