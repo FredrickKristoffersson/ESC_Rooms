5aa497ecad4d18eaf543f0720f06760dd844712d
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2023-11-14 12:21:52 +0100

    Added so that mobile menu-button shows mobile menu

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ const mobileMenu = document.querySelector(".header_menuBtn");
 
 mobileMenu.addEventListener("click", () => {
   console.log("hej det fungerar");
+  document.querySelector(".mobileMenu").classList.add("mobileMenu_open");
+  document.querySelector(".mobileMenu_ul").classList.add("mobileMenu_ul_open");
 });
